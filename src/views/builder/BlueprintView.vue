28a99b4fc90ddf1dc2259e9424e676e6ee5ca49b
Mihó Dániel <miho.daniel@mdy.hu>
2025-09-07 18:42:14 +0200

    fix transition between preview and customize

--- src/views/builder/BlueprintView.vue
+++ src/views/builder/BlueprintView.vue
@@ -93,12 +93,12 @@ const handleTitleCancel = () => {
 
       <!-- Transitioning Content Area -->
       <transition
-        enter-active-class="transition duration-150 ease-out"
-        enter-from-class="opacity-0"
-        enter-to-class="opacity-100"
+        enter-active-class="transition duration-200 ease-out"
+        enter-from-class="opacity-0 translate-y-4"
+        enter-to-class="opacity-100 translate-y-0"
         leave-active-class="transition duration-150 ease-in"
-        leave-from-class="opacity-100"
-        leave-to-class="opacity-0"
+        leave-from-class="opacity-100 translate-y-0"
+        leave-to-class="opacity-0 translate-y-4"
         mode="out-in"
       >
         <!-- 5. Update the main v-if to check the mode value -->
